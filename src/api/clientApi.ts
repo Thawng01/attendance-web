7d0b565fdad28627c541833e87fd115dedf3e037
thechin press <thechinpress@gmail.com>
2025-11-06 15:04:49 +0630

    fixed

--- src/api/clientApi.ts
+++ src/api/clientApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // export const baseURL = "https://attendance-api-xewv.onrender.com/api"
-export const baseURL = "http://localhost:9000/api"
-// export const baseURL = "https://attendanceapi.aridient.com/api"
+// export const baseURL = "http://localhost:9000/api"
+export const baseURL = "https://attendanceapi.aridient.com/api"
 
 export const clientApi = axios.create({
     baseURL,
